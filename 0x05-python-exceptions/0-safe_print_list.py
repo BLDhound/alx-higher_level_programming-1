6b799b79b9f7eddb7531765b5947e82cbc14df73
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2021-09-28 08:17:57 +0300

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    if not my_list or x == 0:
+        print()
+        return(0)
+    try:
+        for index_x in range(x):
+            print(mt_list[index_x], end="")
+    except NameError:
+        index_x = index_x - 1
+    finally:
+        print()
+        return(index_x + 1)
