f0fd74ea17c72de9c958945cba6b8943a2e39a80
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2021-11-17 00:53:31 +0300

    List by best

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- a script that lists all records of the table secong_table
-SELECT score, name FROM second_table ORDER BY score DESC name;
+SELECT score, name FROM second_table ORDER BY score DESC, name;
