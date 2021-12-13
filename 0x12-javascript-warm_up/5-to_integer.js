470b65a8f9b8f4e9df8052ab6e76f49942b047be
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2021-12-13 19:10:38 +0300

    An integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ const process = require('process');
 const arg1 = process.argv[2];
 
 if (parseInt(arg1)) {
-  console.log('My number = ' + parseInt(arg1));
+  console.log('My number: ' + parseInt(arg1));
 } else {
   console.log('Not a number');
 }
