eb2afcc417a54e2a011ad65faefb4cbf24b284a8
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2021-12-13 18:51:09 +0300

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const process = require('process');
+let l = '';
+let i = 0;
+
+if (process.argv.length > 2) {
+  const size = process.argv[2];
+  for (let x = 0; x < size; x++) {
+    while (i < size) {
+      l += 'X';
+      i++;
+    }
+    console.log(l);
+  }
+} else {
+  console.log('Missing size');
+}
