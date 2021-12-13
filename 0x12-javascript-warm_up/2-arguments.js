3da96fada87e43db10a51296096c3972de0753e7
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2021-12-13 19:26:44 +0300

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,11 @@
 const process = require('process');
 
 if (process.argv.length > 2) {
+  if (process.argv.length > 2 && process.argv.length < 4){
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
 } else {
   console.log('No argument');
 }
