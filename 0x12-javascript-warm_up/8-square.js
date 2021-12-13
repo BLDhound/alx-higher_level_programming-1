162435438beadf0786f6803b881483cbe15ec144
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2021-12-13 18:55:28 +0300

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ const process = require('process');
 let l = '';
 let i = 0;
 
-if (process.argv.length > 2) {
+if (process.argv.length > 2 && process.argv[2] == true) {
   const size = process.argv[2];
   for (let x = 0; x < size; x++) {
     while (i < size) {
