9ec1b907d0b995278eed2ede688c2fb4ada6a149
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2021-12-14 05:23:49 +0300

    Advanced

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,16 +1,6 @@
 #!/usr/bin/node
-const process = require('process');
-
-const fac = parseInt(process.argv[2]);
-let aux = fac;
-let aux1 = aux;
-
-if (fac) {
-  while (aux > 1) {
-    aux -= 1;
-    aux1 = aux1 * aux;
-  }
-  console.log(aux1);
-} else {
-  console.log(1);
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
+
+console.log(factorial(Number(process.argv[2])));
