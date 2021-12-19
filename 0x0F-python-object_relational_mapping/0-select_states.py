b9aee25d36778bde43a12629e01a31c90376ef96
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2021-12-20 00:20:38 +0300

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=argv[1],
-                         password=argv[2],
-                         database=argv[3])
+                         passwd=argv[2],
+                         db=argv[3])
 
     # create a cursor to exec queries using SQL
     cursor = db.cursor()
