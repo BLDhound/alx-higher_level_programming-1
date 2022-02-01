5740d83d2f7d9aa13b5e86b8aabfa970b6a12a97
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-02-01 22:45:18 +0300

    Right order

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,23 +1,26 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
-request(url, function (err, response, body) {
+const url = 'https://swapi-api.hbtn.io/api/films/';
+const ep = process.argv[2];
+request(url + ep, function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
-    const completed = {};
-    const tasks = JSON.parse(body);
-    for (const user in tasks) {
-      const task = tasks[user];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
+    const jsobj = JSON.parse(body);
+    console.log(jsobj.characters);
+    for (const index in jsobj.characters) {
+      const charusr = jsobj.characters[index];
+      request(charusr, function (err, response, body) {
+        if (err) {
+          console.log(err);
+        } else if (response.statusCode === 200) {
+          const character = JSON.parse(body);
+          console.log(character.name);
         } else {
-          completed[task.userId]++;
+          console.log('error ocurred, Status code:' + response.statusCode);
         }
-      }
+      });
     }
-    console.log(completed);
   } else {
     console.log('error ocurred, Status code: ' + response.statusCode);
   }
