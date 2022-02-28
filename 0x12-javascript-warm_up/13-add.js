53ca4a3c14f3827e777d04597eb98db132a426b0
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-02-28 22:13:37 +0300

    Correction

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 function add (param1, param2) {
-  return param1 + param2;
+  return (param1 + param2);
 }
-
-console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+exports.add = add;
