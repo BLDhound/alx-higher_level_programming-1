58ddf9acc71f096bd69e0c50ca011dc1f36dbe04
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-04-20 20:17:10 +0300

    Say Hello!

---
+++ 0x015-javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data, textStatus) {
+        if (textStatus === 'success'){
+            $('#hello').text(data.hello);
+        } 
+    });
+});
